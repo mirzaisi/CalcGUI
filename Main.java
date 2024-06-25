9a7cb4a0a1b6ff430a06821241e6afbf3193f69f
Mirza <mirza.isi@std.ieu.edu.tr>
2024-06-25 23:27:05 +0300

    Initialized basic classes

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+*@author mirzaisi
+*@version 1.0
+*@app CalcGUI
+*
+*An ordinary calculator app with GUI and scientific calculation support 
+**/
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
